12b8447d37fd2dddab684c0c1eaf2736d27c51c8
Steven Lum <stevielum1@gmail.com>
2018-07-12 15:00:55 -0700

    Add ugly lyrics helper

--- music_app/app/helpers/application_helper.rb
+++ music_app/app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def ugly_lyrics(lyrics)
+    lyrics = lyrics.split("\n").map { |line| "&#9835;".html_safe + h(" #{line}") }
+
+    html = "<pre>"
+    lyrics.each { |line| html += line + "\n" }
+    html.html_safe
+  end
 end
