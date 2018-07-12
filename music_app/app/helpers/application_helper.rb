83b1b7b2a4a957ab3e242ba7dd0c657b7b09f56f
Steven Lum <stevielum1@gmail.com>
2018-07-12 16:33:38 -0700

    Update buttons

--- music_app/app/helpers/application_helper.rb
+++ music_app/app/helpers/application_helper.rb
@@ -3,7 +3,11 @@ module ApplicationHelper
     lyrics = lyrics.split("\n").map { |line| "&#9835;".html_safe + h(" #{line}") }
 
     html = "<pre>"
-    lyrics.each { |line| html += line + "\n" }
+    lyrics.each do |line|
+      html += line
+      html += "\n" unless line == lyrics.last
+    end
+    html += "</pre>"
     html.html_safe
   end
 end
